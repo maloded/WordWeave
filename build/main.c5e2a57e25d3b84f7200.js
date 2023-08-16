c1ff3e13f5e178b6a798031040a503e1c1e0d33e
maloded <damatomxxx@gmail.com>
2023-08-16 17:45:38 +0300

    Add .env

---
+++ build/main.c5e2a57e25d3b84f7200.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";console.log("Hello!!!"),10..toString()})();
--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -3,6 +3,7 @@ import { Configuration } from 'webpack';
 import { buildPlugins } from './buildPlugins';
 import { buildLoaders } from './buildLoaders';
 import { buildResolvers } from './buildResolvers';
+import { buildDevServer } from './buildDevServer';
 
 export function buildWebpackConfig(options: BuildOptions): Configuration {
   const { mode, paths: { entry, build }, isDev } = options;
@@ -20,5 +21,7 @@ export function buildWebpackConfig(options: BuildOptions): Configuration {
       clean: true,
     },
     plugins: buildPlugins(options),
+    devtool: isDev ? 'inline-source-map' : undefined,
+    devServer: isDev ? buildDevServer(options) : undefined,
   };
 };
