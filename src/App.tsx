bbab752be670d2c5fc79f2fbf42e5c778bd24edc
maloded <damatomxxx@gmail.com>
2023-08-17 13:00:47 +0300

    Impl theme switcher

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,17 @@ import { Route, Routes } from 'react-router-dom';
 import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
 import { MainPageAsync } from './pages/MainPage/MainPage.async';
 import { Link } from 'react-router-dom';
-import './index.scss';
-import { Suspense } from 'react';
+import './styles/index.scss';
+import { Suspense, useContext } from 'react';
+import { Theme, ThemeContext } from './theme/ThemeContext';
+import { useTheme } from './theme/useTheme';
 
 const App = () => {
+  const { theme, toggleTheme } = useTheme();
+
   return (
-    <div className='app'>
+    <div className={`app ${theme}`}>
+      <button onClick={toggleTheme}>Toggle Theme</button>
       <Link to={'/'}>Main</Link>
       <Link to={'/about'}>About</Link>
       <Suspense fallback={<div>Loading...</div>}>
