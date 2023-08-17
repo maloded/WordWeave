de0267eea7e267c5b9bede872a945e079a4fbfe7
maloded <damatomxxx@gmail.com>
2023-08-17 14:17:21 +0300

    Impl classNames

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,15 @@ import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
 import { MainPageAsync } from './pages/MainPage/MainPage.async';
 import { Link } from 'react-router-dom';
 import './styles/index.scss';
-import { Suspense, useContext } from 'react';
-import { Theme, ThemeContext } from './theme/ThemeContext';
+import { Suspense } from 'react';
 import { useTheme } from './theme/useTheme';
+import { classNames } from './helpers/classNames/classNames';
 
 const App = () => {
   const { theme, toggleTheme } = useTheme();
 
   return (
-    <div className={`app ${theme}`}>
+    <div className={classNames('app', {}, [theme])}>
       <button onClick={toggleTheme}>Toggle Theme</button>
       <Link to={'/'}>Main</Link>
       <Link to={'/about'}>About</Link>
