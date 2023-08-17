5b6674fa3f7f8a1ce4829d51fe10a0a55afb04e0
maloded <damatomxxx@gmail.com>
2023-08-17 10:57:45 +0300

    Add routing and lazy pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import { Counter } from './components/Counter';
+import { Route, Routes } from 'react-router-dom';
+import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
+import { MainPageAsync } from './pages/MainPage/MainPage.async';
+import { Link } from 'react-router-dom';
 import './index.scss';
+import { Suspense } from 'react';
 
 const App = () => {
   return (
     <div className='app'>
-      Hello
-      <Counter />
+      <Link to={'/'}>Main</Link>
+      <Link to={'/about'}>About</Link>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path={'/about'} element={<AboutPageAsync />} />
+          <Route path={'/'} element={<MainPageAsync />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 };
---
+++ src/pages/AboutPage/AboutPage.tsx
@@ -0,0 +1,12 @@
+import { Counter } from '../../components/Counter';
+
+
+const AboutPage = () => {
+  return (
+    <div>
+      <Counter />
+    </div>
+  );
+};
+
+export default AboutPage;
