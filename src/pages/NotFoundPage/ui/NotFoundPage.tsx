537c06e861665aecd4fc5120db31999399f6d63f
maloded <damatomxxx@gmail.com>
2023-08-19 16:20:03 +0300

    Add Loader page

---
+++ src/pages/NotFoundPage/ui/NotFoundPage.tsx
@@ -0,0 +1,17 @@
+import { classNames } from 'shared/lib/classNames/classNames';
+import css from './NotFoundPage.module.scss';
+import { useTranslation } from 'react-i18next';
+
+interface NotFoundPageProps {
+  className?: string;
+}
+
+export const NotFoundPage = ({ className }: NotFoundPageProps) => {
+  const { t } = useTranslation();
+
+  return (
+    <div className={classNames(css.NotFoundPage, {}, [className])}>
+      {t('Not found')}
+    </div>
+  );
+};
